b52cac4cf6d568164c6f2efdfd5dc61771a05d8b
Javier Alarcon <ja.ge.al.al@pragma.com.co>
2022-06-09 18:22:53 -0500

    fetching data with loader

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,6 +1,33 @@
-import { Link } from '@remix-run/react'
+import { Link, useLoaderData } from '@remix-run/react'
+
+/* se ejecuta en el servidor para recuperar la data */
+export const loader = () => {
+  const data = {
+    posts: [
+      {
+        id: 1,
+        title: 'Post 1',
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+      },
+      {
+        id: 2,
+        title: 'Post 2',
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+      },
+      {
+        id: 3,
+        title: 'Post 3',
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+      }
+    ]
+  }
+  return data
+}
 
 export default function Index() {
+  /* se usa para obtener los datos que devuelve el loader */
+  const { posts } = useLoaderData()
+
   return (
     <div>
       <h1>Remix Posts</h1>
@@ -18,6 +45,12 @@ export default function Index() {
           </li>
         </ul>
       </nav>
+      {posts.map(post => (
+        <div key={post.id}>
+          <p>{post.title}</p>
+          <p>{post.content}</p>
+        </div>
+      ))}
     </div>
   )
 }
