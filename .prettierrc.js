752a6fd24d62ba042fd1d1a0b427df320f720a0f
Javier Alarcon <ja.ge.al.al@pragma.com.co>
2022-06-09 17:38:36 -0500

    implement eslint and prettier

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+// see https://prettier.io/docs/en/options.html
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  arrowParens: 'avoid', // Omit parens when possible. Example: x => x
+  semi: false,
+  endOfLine: 'auto',
+  spa,
+}
--- app/root.jsx
+++ app/root.jsx
@@ -4,28 +4,41 @@ import {
   Meta,
   Outlet,
   Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+  ScrollRestoration
+} from '@remix-run/react'
 
 export const meta = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
-});
+  charset: 'utf-8',
+  title: 'New Remix App',
+  viewport: 'width=device-width,initial-scale=1'
+})
+
+function Layout() {
+  return (
+    <>
+      <header>
+        <h1>Remix Posts</h1>
+      </header>
+      <Outlet />
+      <footer>Copyrigth 2022</footer>
+    </>
+  )
+}
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang="es">
       <head>
+        <title>Remix Posts</title>
         <Meta />
         <Links />
       </head>
       <body>
-        <Outlet />
+        <Layout />
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
       </body>
     </html>
-  );
+  )
 }
