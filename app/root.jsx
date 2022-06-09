57d59ea659b0b7a094cdf7f6a868516656310fa6
Javier Alarcon <ja.ge.al.al@pragma.com.co>
2022-06-09 17:49:05 -0500

    aply styles

--- app/root.jsx
+++ app/root.jsx
@@ -7,11 +7,20 @@ import {
   ScrollRestoration
 } from '@remix-run/react'
 
+import globalStyles from './styles/global.css'
+
 export const meta = () => ({
   charset: 'utf-8',
-  title: 'New Remix App',
+  title: 'Remix Posts',
   viewport: 'width=device-width,initial-scale=1'
 })
+
+export const links = () => ([
+  {
+    rel: 'stylesheet',
+    href: globalStyles
+  }
+])
 
 function Layout() {
   return (
@@ -29,7 +38,6 @@ export default function App() {
   return (
     <html lang="es">
       <head>
-        <title>Remix Posts</title>
         <Meta />
         <Links />
       </head>
