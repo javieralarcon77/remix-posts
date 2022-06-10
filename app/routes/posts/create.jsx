7bd1f28b7949a93a5d7f4d82d5e6a94142dbabe2
Javier Alarcon <ja.ge.al.al@pragma.com.co>
2022-06-10 09:58:00 -0500

    add create post

--- app/routes/posts/create.jsx
+++ app/routes/posts/create.jsx
@@ -1,13 +1,24 @@
 import { redirect } from '@remix-run/node'
+import { db } from '../../services/db'
 
 export async function action({ request }) {
   const form = await request.formData()
   const title = form.get('title')
   const body = form.get('body')
+  const data = { title, body }
 
-  console.log({ title, body })
+  await db.post.create({ data })
 
   return redirect('/')
+}
+
+export const ErrorBoundary = ({ error }) => {
+  return (
+    <>
+      <h2>Create new post</h2>
+      <p>Algo salio mal lo sentimos {error.message}</p>
+    </>
+  )
 }
 
 export default function CreatePost() {
@@ -17,11 +28,11 @@ export default function CreatePost() {
       <form method="POST">
         <div>
           <label htmlFor="title">Title</label><br/>
-          <input type="text" id="title" name="title"/>
+          <input type="text" id="title" name="title" required/>
         </div>
         <div>
-          <label htmlFor="body">Content</label><br/>
-          <textarea type="text" id="body" name="body"/>
+          <label htmlFor="body">Body</label><br/>
+          <textarea type="text" id="body" name="body" required/>
         </div>
         <button type="submit">Add new Post</button>
       </form>
